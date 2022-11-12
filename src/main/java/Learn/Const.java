a6942415477654046dea2fd6139b4975c30aebd6
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-11-12 18:45:39 +0530

    Initial commit

--- src/main/java/Learn/Const.java
+++ src/main/java/Learn/Const.java
@@ -1,9 +1,14 @@
 package Learn;
 
+
 public class Const {
 
-    public Const(){
+    public static void print(){
+        System.out.println("hhh");
+    }
 
+    public static void main(String[] args){
+        System.out.println(args[0]);
     }
 
 }
