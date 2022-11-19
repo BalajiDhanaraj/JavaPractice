4bce79813eb4c698f17ac1bd18ecbf7d2f33f494
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-11-19 16:03:53 +0530

    Initial commit

--- src/main/java/Learn/Const.java
+++ src/main/java/Learn/Const.java
@@ -3,12 +3,10 @@ package Learn;
 
 public class Const {
 
-    public static void print(){
-        System.out.println("hhh");
-    }
-
     public static void main(String[] args){
-        System.out.println(args[0]);
-    }
+        Dog mydog = new Dog();
 
+
+
+    }
 }
---
+++ src/main/java/Learn/Dog.java
@@ -0,0 +1,12 @@
+package Learn;
+
+public class Dog {
+    String name;
+    int age;
+
+    public Dog(){
+        System.out.println("I'm defautl constructor");
+    }
+
+
+}
