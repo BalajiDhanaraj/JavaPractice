ab27f443056af0c9ec0a18f0acd3f168742eb937
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-11-19 16:18:18 +0530

    Initial commit

--- src/main/java/Learn/Const.java
+++ src/main/java/Learn/Const.java
@@ -4,12 +4,14 @@ package Learn;
 public class Const {
 
     public static void main(String[] args){
-        Dog mydog = new Dog("Hello");
-        System.out.println(mydog.name+" this is one parameter or one args contractor");
+//        Dog mydog = new Dog("Hello");
+//        System.out.println(mydog.name+" this is one parameter or one args contractor");
+//
+//        Dog mydogs = new Dog(5);
+//        System.out.println(mydogs.age+" this is one parameter or one args contractor");
 
-        Dog mydogs = new Dog(5);
-        System.out.println(mydogs.age+" this is one parameter or one args contractor");
-
+        Dog set = new Dog("balaji ",23);
+        System.out.println(set.name+" "+set.age);
 
 
     }
--- src/main/java/Learn/Dog.java
+++ src/main/java/Learn/Dog.java
@@ -4,17 +4,21 @@ public class Dog {
     String name;
     int age;
 
-    public Dog(){
-        System.out.println("I'm defautl constructor");
-    }
+//    public Dog(){
+//        System.out.println("I'm defautl constructor");
+//    }
 
-    public Dog(String name){
-        this.name = name;
-    }
+//    public Dog(String name){
+//        this.name = name;
+//    }
+//
+//    public Dog(int age){
+//        this.age = age;
+//    }
 
-    public Dog(int age){
-        this.age = age;
+    public Dog(String name,int age){
+      this.name = "balaji";
+      this.age = 23;
     }
-
 
 }
