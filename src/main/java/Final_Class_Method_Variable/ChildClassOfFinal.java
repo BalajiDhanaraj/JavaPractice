ff21baae26cdb07f4ca6ba51d8490ea4c936759c
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-11-19 18:13:39 +0530

    Initial commit

--- src/main/java/Final_Class_Method_Variable/ChildClassOfFinal.java
+++ src/main/java/Final_Class_Method_Variable/ChildClassOfFinal.java
@@ -3,15 +3,15 @@ package Final_Class_Method_Variable;
 public class ChildClassOfFinal extends ParentClassOfFinalAnimal {
 
 
-//    @Override
-//    public void eat(){
-//        System.out.println("Child is eating nwo "+ParentClassOfFinalAnimal.PI_VALUE);
-//    }
-//
-//    public void childEatingNow(){
-//        System.out.println("Child is eating nwo");
-//    }
-//
+    @Override
+    public void eat(){
+        System.out.println("Child is eating nwo "+ParentClassOfFinalAnimal.PI_VALUE);
+    }
+
+    public void childEatingNow(){
+        System.out.println("Child is eating nwo");
+    }
+
 
 
 
