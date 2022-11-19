69c381846c741cd57d56bdb7f2a31078ca7e34d5
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-11-19 16:08:13 +0530

    Initial commit

--- src/main/java/Learn/Const.java
+++ src/main/java/Learn/Const.java
@@ -4,7 +4,9 @@ package Learn;
 public class Const {
 
     public static void main(String[] args){
-        Dog mydog = new Dog();
+        Dog mydog = new Dog("Hello");
+        System.out.println(mydog.name+" this is one parameter or one args contructor");
+
 
 
 
--- src/main/java/Learn/Dog.java
+++ src/main/java/Learn/Dog.java
@@ -8,5 +8,12 @@ public class Dog {
         System.out.println("I'm defautl constructor");
     }
 
+    public Dog(String name){
+        this.name = name;
+    }
+
+    public Dog(int age){
+        this.age = age;
+    }
 
 }
