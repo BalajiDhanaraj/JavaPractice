7b9f7a3dab163659ddc29324faa6e8bc2cdbdddd
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-11-19 16:56:20 +0530

    Initial commit

--- src/main/java/Learn/Dog.java
+++ src/main/java/Learn/Dog.java
@@ -4,23 +4,28 @@ public class Dog {
     String name;
     int age;
 
-//    public Dog(){
-//        System.out.println("I'm defautl constructor");
-//    }
+    // this the default contractor it will call by java when the object (new) is created
+    // when we use declare the constructor manually java will call the constructor only
+    // it does not create the new constructor
+    public Dog(){
+        System.out.println("I'm defautl constructor");
+    }
 
+    // this is the default constructor but the values are assign to the field
       public Dog(){
           name = "mani";
           age = 24;
       }
 
-//    public Dog(String name){
-//        this.name = name;
-//    }
-//
-//    public Dog(int age){
-//        this.age = age;
-//    }
+    public Dog(String name){
+        this.name = name;
+    }
 
+    public Dog(int age){
+        this.age = age;
+    }
+
+    // this is the parameterized constructor with two args
     public Dog(String name,int age){
       this.name = "balaji";
       this.age = 23;
