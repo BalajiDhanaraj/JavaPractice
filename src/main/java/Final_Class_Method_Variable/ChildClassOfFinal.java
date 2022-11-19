bcae59e4c4ed090f9a40bc61de9561e2e8e9b50a
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-11-19 17:43:24 +0530

    Initial commit

--- src/main/java/Final_Class_Method_Variable/ChildClassOfFinal.java
+++ src/main/java/Final_Class_Method_Variable/ChildClassOfFinal.java
@@ -3,10 +3,10 @@ package Final_Class_Method_Variable;
 public class ChildClassOfFinal extends ParentClassOfFinalAnimal {
 
 
-//    @Override
-//    public void eat(){
-//        System.out.println("Child is eating nwo");
-//    }
+    @Override
+    public void eat(){
+        System.out.println("Child is eating nwo");
+    }
 
     public void childEatingNow(){
         System.out.println("Child is eating nwo");
--- src/main/java/Final_Class_Method_Variable/ParentClassOfFinalAnimal.java
+++ src/main/java/Final_Class_Method_Variable/ParentClassOfFinalAnimal.java
@@ -1,9 +1,16 @@
 package Final_Class_Method_Variable;
 
-public class ParentClassOfFinalAnimal {
+/// when initialize the make the class as final --> its will prevent the subclasses to inherit
+// public final class ParentClassOfFinalAnimal
+public  class ParentClassOfFinalAnimal {
 
+    // Declare the Constant in the java
+    // final variable --> no one can change or reassign the declared values so we can anywhere we want
+    public static final double PI_VALUE =  3.14343;
 
-    public void eat(){
+    // when u declare the method as final --> we cant override the method in child classes to prevent
+    // overriding ------> public final void eat(){
+    public  void eat(){
         System.out.println("eat");
     }
 
