3256b193b1b5d659884e834c75690da65abb3796
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2024-10-21 17:36:55 +0530

    Create Fibo.java

---
+++ src/main/java/DSA/Fibo.java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter the number");
+      int a = 0;
+      int b = 1;
+      int count = 2;
+
+      while(count<=num){
+        int temp = b;
+        int b = b + a;
+        int a = temp;
+
+        count++;
+      }
+      
+      system.out.println(b);
+    }
+}
