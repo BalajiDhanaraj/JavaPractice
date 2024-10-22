de8b5030995389eea4df0343274c8155254de499
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2024-10-22 11:30:47 +0530

    Update Temp.java

--- src/main/java/DSA/Temp.java
+++ src/main/java/DSA/Temp.java
@@ -1,7 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.util.*;
-class HelloWorld {
+class Temp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number to check prime or not");
