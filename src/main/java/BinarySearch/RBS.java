9a16a1b153380227c8321208792e072756646a77
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2024-11-24 18:19:33 +0530

    Update RBS.java

--- src/main/java/BinarySearch/RBS.java
+++ src/main/java/BinarySearch/RBS.java
@@ -102,6 +102,50 @@
      return -1;
    }
 
+  static int FindThePivotDuplicate(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        // If the array is rotated
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+           
+          // if(start==end && end==mid){
+          //     return arr[mid];
+          // }
+
+            // Check if mid is the maximum
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return arr[mid];
+            }
+            // Check if mid-1 is the maximum
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return arr[mid - 1];
+            }
+
+            // Decide which half to continue searching
+            if (arr[start] == arr[mid] && arr[mid]==arr[end]) {
+              
+              if(start<end && arr[start]>arr[start+1]){
+                return arr[start];
+              }
+              start++;
+              if(end>start && arr[end]<arr[end-1]){
+                return arr[end-1];
+              }
+              end--;
+               
+            }else if (arr[start]<arr[end] || arr[start]==arr[mid] && arr[mid]>arr[end]) {
+               // Search in the right half
+               start = mid+1;
+            }else {
+                end = mid - 1; // Search in the left half
+            }
+        }
+
+        return -1; // This line should never be reached if the array is valid
+    }
+    
    
   
 }
