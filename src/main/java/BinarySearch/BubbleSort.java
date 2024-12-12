c1a1f977ddb22de3fa9a0e3c7783b88f6c7f554d
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2024-12-12 13:20:50 +0530

    Create BubbleSort.java

---
+++ src/main/java/BinarySearch/BubbleSort.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+  
+  
+    public static void main(String[] args) {
+      
+      int[] arr = {1,2,4,3,5};
+      
+      int[] ans = BubbleSort(arr);
+      System.out.println(Arrays.toString(ans));
+      
+  }
+  
+  
+  public static int[] BubbleSort(int[] arr){
+    
+    boolean swapped;
+    
+    for (int i=0;i<arr.length;i++){
+      swapped = false;
+      for (int j=1;j<arr.length-i;j++){
+        
+        if (arr[i]<arr[j]){
+          
+          int temp = arr[i];
+          arr[i] = arr[j];
+          arr[j-1] = temp;
+          
+          swapped = true;
+          
+        } 
+      }
+      
+      if(!swapped){
+        break;
+      }
+      
+    } 
+    
+    
+    
+    
+    return arr;
+  }
+  
+  
+}
